887f055defe7b82704155b0f124883925cb920ad
Moyrasc <noemisolac@gmail.com>
2023-09-15 18:21:51 +0200

    add download CV

--- src/views/SobreMi.jsx
+++ src/views/SobreMi.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 // import Particles from '../components/Particles'
 import { ContainerSobre, Img, MainContent, SubTitle, TextContent } from './SobremiStyled'
+import { Btn } from './ContactoStyled'
+import { Link } from 'react-router-dom'
+import CV from '../CV.pdf'
 
 const SobreMi = () => {
   return (
@@ -11,8 +14,13 @@ const SobreMi = () => {
         <img src='https://picsum.photos/250/250?random=1' alt=''/>
       </Img>
       <TextContent>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu orci at dui lacinia gravida. Donec commodo dolor sed enim maximus lacinia. Mauris feugiat elit vitae ultrices placerat. Donec efficitur justo nec erat gravida pharetra. Maecenas diam nibh, tristique vel molestie in, tincidunt vitae ligula. Pellentesque id tellus id dolor ornare vehicula. Nam magna purus, tempus ac est sit amet, interdum convallis mauris. Aenean sodales quis leo non viverra. Ut urna magna, porttitor id ultrices pulvinar, lacinia sit amet eros. Duis at dictum nisl, sed molestie quam. Praesent eu nibh consectetur, blandit sapien fringilla, sollicitudin nisl. Vestibulum congue lacus ipsum. Proin ultrices tincidunt ultricies.</p>
+        <p>Mi historia es un viaje de transformación desde una carrera en enfermería hacia el desarrollo de software.<br/>
+          Como enfermera, mi trabajo se centraba en cuidar a las personas y brindarles atención médica de calidad en momentos críticos. A lo largo de los años, aprendí la importancia de la empatía, la paciencia y la resolución de problemas. Estas habilidades han demostrado ser invaluables en mi transición a la programación.<br/>
+          La programación para mí es como un rompecabezas interminable. Me encanta enfrentarme a desafíos complejos y encontrar soluciones creativas a problemas. Mi curiosidad me impulsa a aprender constantemente y a explorar nuevas tecnologías.
+        </p>
       </TextContent>
+      <Link to={CV} download="CV-Noemi-Sola" target='_blank' rel='noreferrer'><Btn> Descargar CV</Btn></Link>
+      
     </MainContent>
     {/* <Particles id="tsparticles"/> */}
     </ContainerSobre>
